656870c92866da03a50f57663184eb72f4d5687e
JoFrhwld <JoFrhwld@gmail.com>
2023-01-19 18:48:58 -0500

    fonts

---
+++ font_install.R
@@ -0,0 +1,12 @@
+library(gfonts)
+setup_font(
+  id = "comfortaa",
+  output_dir = "www",
+  prefer_local_source = T
+)
+
+setup_font(
+  id = "atkinson-hyperlegible",
+  output_dir = "www",
+  prefer_local_source = T
+)
